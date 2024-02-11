2d33bb2b5ba6ab15aa320c086421e176e8958572
Lili0009 <lianjaspeo1009@gmail.com>
2024-02-11 14:00:14 +0800

    Create description.php

---
+++ description.php
@@ -0,0 +1,119 @@
+<?php
+include 'header.php';
+include("includes/dbh.inc.php");
+
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $customer_id = $_SESSION['userId'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = 1;
+
+   $select_cart = mysqli_query($con, "SELECT * FROM `cart` WHERE name = '$product_name' AND customer_id = '$customer_id'");
+
+   if(mysqli_num_rows($select_cart) > 0){
+      $message[] = 'product already added to cart';
+   }else{
+      $insert_product = mysqli_query ($con, "INSERT INTO cart(customer_id, name, price, image, quantity) VALUES($customer_id, '$product_name', '$product_price', '$product_image', '$product_quantity')");
+      $message[] = 'product added to cart succesfully';
+   }
+
+
+}
+?>
+
+
+
+
+<title>Products</title>
+<header>
+        <nav>
+        <?php
+          if(isset($_SESSION['userUId'])){
+            if($_SESSION['userUId'] == 'admin'){
+            echo '<a href="index_admin.php"><img class = "image-logo" src="logo.png"></a>';
+            }
+            else{
+              echo '<a href="index_cust.php"><img class = "image-logo" src="logo.png"></a>';
+            }
+          }
+        ?>
+           <ul class="head-ul">
+              
+   <?php
+	if(isset($_SESSION['userUId'])){
+		if($_SESSION['userUId'] == 'admin'){
+			echo '
+         <li><a href="index_admin.php">Dashboard</a></li>
+         <li><a class = "active" href="Products.php">Customer View</a></li>
+         <li><a href ="admin.php">ADMIN</a></li>
+			<li><a href ="orders.php"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-clipboard-check-fill" viewBox="0 0 16 16">
+            <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3Zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3Z"/>
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5v-1Zm6.854 7.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708Z"/>
+            </svg><span class="ordernum"> '.$order_count.'</span></a></li>
+			<li><a href="logout.php"">LOG OUT</a></li>';
+		}
+		else{
+
+            echo '
+            <li><a href="index_cust.php">Home</a></li>
+              <li><a class = "active" href="Products.php">Products</a></li>
+              <li><a href="about.php">About</a></li>
+            <li><a href ="cart.php"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+          </svg><span class="cartnum"> '.$ct.'</span></a></li>
+          
+				<div class="dropdown">
+                        <li><a><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                      </svg></button></a></li>
+                        <div class = "dropdown-content">
+                                <a href="profile.php"><center>MY PROFILE</center></a>
+								<a href="customers_order.php"><center>ORDERS</center></a>
+                                <a href="logout.php"><center>LOG OUT</center></a>
+                        </div>
+                    </div>';
+		}
+	}
+    
+	else{
+		echo '<li><a href ="about.php">ABOUT</a></li>
+		<div class="dropdown">
+                        <li><a>ACCOUNT</button></a></li>
+                        <div class = "dropdown-content">
+                                <a href="login.php"><center>LOG IN</center></a>
+                                <a href="signup.php"><center>SIGN UP</center></a>
+                        </div>
+                    </div>';
+	}
+       
+	
+	?>
+           </ul>
+        </nav>
+</header>
+<style>
+.inf{
+    font-size: 20px;
+}
+</style>   
+<?php
+if(isset($message)){
+   echo '<div class="message-container">';
+   foreach($message as $msg){
+      $class = (strpos($msg, 'error') !== false) ? 'error' : '';
+      echo '<div class="message ' . $class . ' small-font"><span>'.$msg.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i></div>';
+   };
+   echo '</div>';
+}
+?>
+<?php
+if(isset($_POST['info'])){
+    $name = $fetch_product['product_name'];
+    echo'$name';
+};
+?>
+<br><br><br>
+
